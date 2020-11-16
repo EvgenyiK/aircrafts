77bf210ae6ccb701d91ba21024f8ca224968d591
Evgen <evgenprogrammer@gmail.com>
2020-11-16 17:52:43 +0300

    функционал

--- api/main.go
+++ api/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"github.com/rs/cors/wrapper/gin"
+	routes "aircrafts/api/routes"
+)
+
 func main() {
-	
+	router:= gin.Default()
+	routes
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module aircrafts
 
 go 1.15
+
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/rs/cors v1.7.0
+)
