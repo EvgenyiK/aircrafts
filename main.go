1bfe74dd6172e9a8a5d4b1eb09d496e02e5a88d7
Evgen <evgenprogrammer@gmail.com>
2020-11-17 16:48:50 +0300

    restruct  project

--- main.go
+++ main.go
@@ -4,12 +4,13 @@ import (
 	"log"
 	"net/http"
 
-	//routes "github.com/EvgenyiK/aircrafts/api/routes"
+	"github.com/EvgenyiK/aircrafts/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router:= gin.Default()
+	models.Connect()
 	router.LoadHTMLGlob("templates/*")
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{
